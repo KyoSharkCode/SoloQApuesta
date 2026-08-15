59da05f598d0191a5ff95edcd689965188e635fe
KyoSharkCode <blayder2002@gmail.com>
2026-08-15 13:30:07 +0200

    Update actualizar_datos.py
    Primeros jugadores añadidos

--- actualizar_datos.py
+++ actualizar_datos.py
@@ -9,9 +9,10 @@ REGION_GAME = "la1"      # Usa 'la1' para LAN, 'la2' para LAS, 'na1' para NA
 
 # LISTA DE TUS AMIGOS: Reemplaza estos nombres por los de tu grupo (Máximo 5-10 por ahora)
 JUGADORES = [
-    {"name": "AmigoPro", "tag": "LAN"},
-    {"name": "OtroAmigo", "tag": "LAS"},
-    {"name": "TuNombre", "tag": "1234"}
+    {"Kyo": "Galactic Shark", "tag": "AYK"},
+    {"Buñuelo": "El Buñuelito", "tag": "KyA"},
+    {"Pinea": "Pinea", "tag": "Pinea"}
+    {"Román": "ゆうき まこと", "tag": "1411"}
 ]
 
 def obtener_datos():
